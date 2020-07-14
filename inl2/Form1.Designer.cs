221cdd98141d923d9642fbf1a249f8665941c563
VIKTOR LJUNGDAHL <vilj15@student.bth.se>
2020-07-15 00:13:36 +0200

    Add project files.

---
+++ inl2/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace inl2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveAsBtn = new System.Windows.Forms.Button();
+            this.openFile = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.newBtn = new System.Windows.Forms.Button();
+            this.closeBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // saveAsBtn
+            // 
+            this.saveAsBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveAsBtn.Location = new System.Drawing.Point(12, 41);
+            this.saveAsBtn.Name = "saveAsBtn";
+            this.saveAsBtn.Size = new System.Drawing.Size(75, 23);
+            this.saveAsBtn.TabIndex = 0;
+            this.saveAsBtn.Text = "Spara som";
+            this.saveAsBtn.UseVisualStyleBackColor = true;
+            this.saveAsBtn.Click += new System.EventHandler(this.saveAsBtn_Click);
+            // 
+            // openFile
+            // 
+            this.openFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.openFile.Location = new System.Drawing.Point(12, 70);
+            this.openFile.Name = "openFile";
+            this.openFile.Size = new System.Drawing.Size(75, 23);
+            this.openFile.TabIndex = 1;
+            this.openFile.Text = "Öppna";
+            this.openFile.UseVisualStyleBackColor = true;
+            this.openFile.Click += new System.EventHandler(this.openFile_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveBtn.Location = new System.Drawing.Point(12, 12);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(75, 23);
+            this.saveBtn.TabIndex = 2;
+            this.saveBtn.Text = "Spara";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox.Location = new System.Drawing.Point(93, 12);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(367, 228);
+            this.textBox.TabIndex = 3;
+            // 
+            // newBtn
+            // 
+            this.newBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.newBtn.Location = new System.Drawing.Point(12, 99);
+            this.newBtn.Name = "newBtn";
+            this.newBtn.Size = new System.Drawing.Size(75, 23);
+            this.newBtn.TabIndex = 4;
+            this.newBtn.Text = "Ny";
+            this.newBtn.UseVisualStyleBackColor = true;
+            this.newBtn.Click += new System.EventHandler(this.newBtn_Click);
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.Location = new System.Drawing.Point(12, 128);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(75, 23);
+            this.closeBtn.TabIndex = 5;
+            this.closeBtn.Text = "Stänga";
+            this.closeBtn.UseVisualStyleBackColor = true;
+            this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 252);
+            this.Controls.Add(this.closeBtn);
+            this.Controls.Add(this.newBtn);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.openFile);
+            this.Controls.Add(this.saveAsBtn);
+            this.Name = "Form1";
+            this.Text = "Ordbehandling";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button saveAsBtn;
+        private System.Windows.Forms.Button openFile;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button newBtn;
+        private System.Windows.Forms.Button closeBtn;
+    }
+}
+
---
+++ inl2/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace inl2
+{
+    public partial class Form1 : Form
+    {
+        String fileName = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFile_Click(object sender, EventArgs e)
+        { int value = 0;
+            value = checkSaved();
+
+            if (value == 0)
+            {
+                OpenFileDialog dlg = new OpenFileDialog();
+                dlg.Filter = "Text Files (*.txt)|*.txt|All Files (*)|*";
+                var result = dlg.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    var text = File.ReadAllText(dlg.FileName);
+                    textBox.Text = text;
+                    fileName = dlg.FileName;
+                }
+                else
+                {
+                    MessageBox.Show("You pressed cancel");
+                }
+            }
+            else
+            { 
+            }
+        
+           
+        }
+
+      
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+
+            if (fileName == "")
+            {
+                SaveFileDialog dlg = new SaveFileDialog();
+                dlg.Filter = "Text Files (*.txt)|*.txt|All Files (*)|*";
+                var result = dlg.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    File.WriteAllText(dlg.FileName, textBox.Text);
+                    MessageBox.Show("File Saved!");
+                    fileName = dlg.FileName;
+
+                }
+                else
+                {
+                    MessageBox.Show("You pressed cancel");
+                }
+            }
+            else
+            {
+                File.WriteAllText(fileName, textBox.Text); ;
+                MessageBox.Show("File Saved!");
+             
+            }
+
+            
+        } 
+
+        private void saveAsBtn_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "Text Files (*.txt)|*.txt|All Files (*)|*";
+            var result = dlg.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                File.WriteAllText(dlg.FileName, textBox.Text);
+                MessageBox.Show("File Saved!");
+                fileName = dlg.FileName;
+
+            }
+            else
+            {
+                MessageBox.Show("You pressed cancel");
+            }
+        }
+
+        private void newBtn_Click(object sender, EventArgs e)
+        {
+            int value = 0;
+            value = checkSaved();
+            if (value == 0)
+            {
+                textBox.Clear();
+                fileName = "";
+            }
+        }
+
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+            int value = 0;
+            value = checkSaved();
+
+            if (value == 1)
+            {
+
+            }
+            else if (fileName == "" && ((textBox.Text).Length > 0))
+            {
+                var result = MessageBox.Show("Close Program?", "dok1.txt", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+                if (result == DialogResult.Yes)
+                    Close();
+
+            }
+            else if (fileName.Length > 0)
+            {
+                var result = MessageBox.Show("Close Program?", fileName.Substring(48), MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+                if (result == DialogResult.Yes)
+                    Close();
+            }
+            
+        }
+
+        public int checkSaved()
+        {
+            String text = textBox.Text;
+            String fileText = null;
+            int returnValue = 0;
+
+            if (fileName != "")
+            {
+                fileText = File.ReadAllText(fileName);
+              
+            }
+
+            if (text.Length > 0 && fileName == "")
+            {
+                var result = MessageBox.Show("Save Program?", "dok1.txt*", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information);
+             
+                if (result == DialogResult.Cancel)
+                    returnValue = 1;
+                else if (result == DialogResult.Yes)
+                {
+                    SaveFileDialog dlg = new SaveFileDialog();
+                    dlg.Filter = "Text Files (*.txt)|*.txt|All Files (*)|*";
+                    var dialogResult = dlg.ShowDialog();
+
+                    if (dialogResult == DialogResult.OK)
+                    {
+                        File.WriteAllText(dlg.FileName, textBox.Text);
+                        MessageBox.Show("File Saved!");
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("You pressed cancel");
+                    }
+                   
+                }
+            }
+            
+            else if ( (fileText != (textBox.Text).ToString()) && fileName.Length > 0 )
+            {
+                var result = MessageBox.Show("Save Program?", (fileName.Substring(48)+"*"), MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information);
+
+                if (result == DialogResult.Cancel)
+                    returnValue = 1;
+                else if (result == DialogResult.Yes)
+                {
+                    File.WriteAllText(fileName, textBox.Text); ;
+                    MessageBox.Show("File Saved!");
+                  
+                }
+                
+            }
+
+            return returnValue;
+           
+        }
+
+    }
+}
